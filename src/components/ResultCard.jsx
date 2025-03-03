641103644fae77b254794065b97630800a927d9b
Lison_Kas <lisonfortin@icloud.com>
2025-03-03 17:26:50 +0100

    toutes les infos dans ResultCard ok

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -3,7 +3,15 @@ export default function ResultCard({result}) {
     return (
         <div>
             <h2>{result.environment}</h2>
-
+            <p>Temperature : from {result.temperature_min} to {result.temperature_max}°C<br/>
+            Water percentage : from {result.percentage_water_min} to {result.percentage_water_max}%<br/>
+            Salt percentage : from {result.percentage_salt_min} to {result.percentage_salt_max}%<br/>
+            Realism percentage : from {result.percentage_realism_min} to {result.percentage_percentage_max}%</p>
+            <ul> Ressources :
+                {result.ressources.map((resource, index) => (
+                    <li key={index}>{resource}</li>
+                ))}
+            </ul>
         </div>
     )
 }
