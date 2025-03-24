6ae569bd261b72679d5f719df0b5a26a04b86a96
Lison_Kas <lisonfortin@icloud.com>
2025-03-24 14:33:44 +0100

    formulaire responsive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,11 +13,9 @@ export default function Form({ setFormData }) {
     setFormData(updatedData);
   };
 
-  // Fonction de filtrage des résultats en fonction des valeurs de formulaire
   const filterResults = () => {
     let results = worldbuilding.results;
 
-    // Filtrer par température
     if (localFormData.temperature) {
       const temperature = Number(localFormData.temperature);
       results = results.filter(
@@ -26,19 +24,16 @@ export default function Form({ setFormData }) {
       );
     }
 
-    // Filtrer par relief
     if (localFormData.relief) {
       results = results.filter((result) => result.relief === localFormData.relief);
     }
 
-    // Filtrer par ressources
     if (localFormData.ressources && localFormData.ressources.length > 0) {
       results = results.filter((result) =>
         localFormData.ressources.every((resource) => result.ressources.includes(resource))
       );
     }
 
-    // Filtrer par pourcentage d'eau
     if (localFormData.percentage_water) {
       const waterPercentage = Number(localFormData.percentage_water);
       results = results.filter(
@@ -48,7 +43,6 @@ export default function Form({ setFormData }) {
       );
     }
 
-    // Filtrer par pourcentage de sel
     if (localFormData.percentage_salt) {
       const saltPercentage = Number(localFormData.percentage_salt);
       results = results.filter(
@@ -58,7 +52,6 @@ export default function Form({ setFormData }) {
       );
     }
 
-    // Filtrer par pourcentage de réalisme
     if (localFormData.percentage_realism) {
       const realismPercentage = Number(localFormData.percentage_realism);
       results = results.filter(
@@ -68,23 +61,20 @@ export default function Form({ setFormData }) {
       );
     }
 
-    // Mettre à jour les résultats filtrés
     setFilteredResults(results);
   };
 
-  // Utilisation de useEffect pour filtrer les résultats à chaque modification du formulaire
   useEffect(() => {
     filterResults();
   }, [localFormData]);
 
   return (
     <>
-      <form>
+      <form className="form-content">
         {worldbuilding.form_fields.map((field) => (
           <div key={field.name}>
             <label>{field.name.charAt(0).toUpperCase() + field.name.slice(1)}</label>
 
-            {/* Champ de type number (ex : température, pourcentage d'eau, sel, etc.) */}
             {field.type === "number" && (
               <input
                 type="number"
@@ -94,7 +84,6 @@ export default function Form({ setFormData }) {
               />
             )}
 
-            {/* Champ de type sélection (ex : relief) */}
             {field.type === "selection" && !field.options[0]?.ressources_type && (
               <select
                 value={localFormData[field.name] || ""}
@@ -109,14 +98,13 @@ export default function Form({ setFormData }) {
               </select>
             )}
 
-            {/* Champ de type texte (ex : ressources) */}
             {field.type === "text" && (
-              <div>
+              <div className="ressources-titles">
                 <label>Select Resources</label>
                 {field.options.map((resourceCategory) => (
                   <div key={resourceCategory.ressources_type}>
-                    <div>{resourceCategory.ressources_type}</div>
-                    <div>
+                    <div className="ressources-content">{resourceCategory.ressources_type}</div>
+                    <div className="ressources-content">
                       {resourceCategory.options.map((option) => (
                         <label key={option}>
                           <input
--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -7,7 +7,7 @@ export default function ResultCard({result}) {
             <p>Temperature : from {result.temperature_min} to {result.temperature_max}°C<br/>
             Water percentage : from {result.percentage_water_min} to {result.percentage_water_max}%<br/>
             Salt percentage : from {result.percentage_salt_min} to {result.percentage_salt_max}%<br/>
-            Realism percentage : from {result.percentage_realism_min} to {result.percentage_percentage_max}%</p>
+            Realism percentage : from {result.percentage_realism_min} to {result.percentage_realism_max}%</p>
             <ul> Ressources :
                 {result.ressources.map((resource, index) => (
                     <li key={index}>{resource}</li>
